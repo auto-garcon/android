ceb19f4b187a2d456d92e191c76ca40f1c952e3d
Mitchell Nelson <nels4929@stthomas.edu>
2020-02-25 11:13:17 -0600

    Categories Activity

--- app/src/main/java/com/autogarcon/android/CategoryListAdapter.java
+++ app/src/main/java/com/autogarcon/android/CategoryListAdapter.java
@@ -1,14 +1,61 @@
 package com.autogarcon.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class CategoryListAdapter extends AppCompatActivity {
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_list);
+public class CategoryListAdapter extends RecyclerView.Adapter<CategoryListAdapter.MyViewHolder> {
+
+    private List<Category> categoryList;
+    private RecyclerViewClickListener mListener;
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView categoryName;
+        public ImageView categoryImage;
+        private RecyclerViewClickListener mListener;
+
+        public MyViewHolder(View view, RecyclerViewClickListener listener) {
+            super(view);
+            mListener = listener;
+            categoryName = (TextView) view.findViewById(R.id.categoryName);
+            categoryImage = (ImageView) view.findViewById(R.id.categoryImage);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            mListener.onClick(view, getAdapterPosition());
+        }
     }
+
+        public CategoryListAdapter(List<Category> categoryList) {
+            this.categoryList = categoryList;
+        }
+
+        @Override
+        public CategoryListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View categoryView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.category_list, parent, false);
+            return new CategoryListAdapter.MyViewHolder(categoryView, mListener);
+        }
+
+        @Override
+        public void onBindViewHolder(CategoryListAdapter.MyViewHolder holder, int position) {
+            Category category = categoryList.get(position);
+            holder.categoryName.setText(String.valueOf(category.getName()));
+            holder.categoryImage.setImageResource(R.drawable.placeholder);
+        }
+
+        @Override
+        public int getItemCount() {
+            return categoryList.size();
+        }
 }
