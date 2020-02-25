8b69e6ba3045bc1160c723746e931c8bbc51c2c8
Mitchell Nelson <nels4929@stthomas.edu>
2020-02-25 11:13:03 -0600

    Categories Activity

---
+++ app/src/main/java/com/autogarcon/android/CategoryListAdapter.java
@@ -0,0 +1,14 @@
+package com.autogarcon.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryListAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_list);
+    }
+}
---
+++ app/src/main/java/com/autogarcon/android/RecyclerViewClickListener.java
@@ -0,0 +1,4 @@
+package com.autogarcon.android;
+
+public interface RecyclerViewClickListener {
+}
