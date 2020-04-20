2d1d9667297ff80a4762ae899f4f502d41020b7a
Riley Tschumper <riley.tschumper@gmail.com>
2020-04-20 17:41:48 -0500

    Add comments to signin file

--- app/src/main/java/com/autogarcon/android/Signin.java
+++ app/src/main/java/com/autogarcon/android/Signin.java
@@ -15,6 +15,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+/**
+ * Activity to integrate Google Sign-In on the application
+ * MainActivity redirects to this activity once the application loads
+ * @author Riley Tschumper
+ */
 public class Signin extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
@@ -51,25 +56,23 @@ public class Signin extends AppCompatActivity {
         });
     }
 
+    /**
+     * When the activity enters the Started state, the system invokes this callback
+     * Checks for existing signed in account
+     * @author Riley Tschumper
+     */
     protected void onStart(){
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        //updateUI(account);
-    }
-/*
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
-        }
     }
 
- */
+    /**
+     * Invoked by clicking on the Sign-in button
+     * Starts the signInIntent for the client
+     * @author Riley Tschumper
+     */
     private void signIn() {
         error.setText("");
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -89,6 +92,11 @@ public class Signin extends AppCompatActivity {
         }
     }
 
+    /**
+     * Display information on the UI and move onto next activity
+     * @param completedTask a returned Task of GoogleSignInAccount type
+     * @author Riley Tschumper
+     */
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
@@ -97,6 +105,8 @@ public class Signin extends AppCompatActivity {
             ActiveSession.getInstance().setGoogleSignInAccount(account);
             // Signed in successfully, show authenticated UI.
             Log.d("DISPLAY", account.getDisplayName());
+
+            // If a valid token, pass onto the next intent
             Intent intent = new Intent(getApplicationContext(), LandingPageActivity.class);
             startActivity(intent);
         } catch (ApiException e) {
