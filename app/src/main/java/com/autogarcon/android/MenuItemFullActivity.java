8371aa27249f0b83c6f7f06442fd8e842d1a48a2
Tim Callies <timcallies@gmail.com>
2020-05-05 10:04:10 -0500

    Add missing import statement

--- app/src/main/java/com/autogarcon/android/MenuItemFullActivity.java
+++ app/src/main/java/com/autogarcon/android/MenuItemFullActivity.java
@@ -3,6 +3,8 @@ package com.autogarcon.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
